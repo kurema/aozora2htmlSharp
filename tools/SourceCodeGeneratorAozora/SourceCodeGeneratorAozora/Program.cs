455082714160eb39353bda4d106615f741bfad61
kurema <kurema_makoto_software@yahoo.co.jp>
2021-06-21 00:14:44 +0900

    improved

--- tools/SourceCodeGeneratorAozora/SourceCodeGeneratorAozora/Program.cs
+++ tools/SourceCodeGeneratorAozora/SourceCodeGeneratorAozora/Program.cs
@@ -38,13 +38,21 @@ namespace SourceCodeGeneratorAozora
             var regHashDeclare1 = new Regex(@"^:(\w+)\s*\=\>\s*""([^""]*)""\s*,?$");
             var regHashDeclare2 = new Regex(@"^""(\w+)""\s*\=\>\s*""([^""]*)""\s*,?$");
             var regComment = new Regex("^#(.*)$");
+            var regCommentPartial = new Regex(@"#([^""]*)$");
 
             var structure = new CodeStructure();
 
             while ((line = await sr.ReadLineAsync()) != null)
             {
                 string lineOrg = line;
-                line = Regex.Replace(line.Trim(), @"#.*$", "").Replace(".to_sjis", "");
+                line = line.Trim();
+                string lineComment = "";
+                {
+                    var matchCommentP = regCommentPartial.Match(line);
+                    lineComment = matchCommentP.Success ? matchCommentP.Groups[1].Value : "";
+                    line = regCommentPartial.Replace(line, "");
+                }
+                line = line.Replace(".to_sjis", "");
                 line = lineLast + line;
                 lineLast = "";
 
@@ -57,7 +65,7 @@ namespace SourceCodeGeneratorAozora
 
                 if (string.IsNullOrWhiteSpace(line))
                 {
-                    await source.AddWithIndent("");
+                    await source.Add("");
                     continue;
                 }
 
@@ -120,7 +128,15 @@ namespace SourceCodeGeneratorAozora
                             }
                         case "end":
                             {
-                                if (structure.Count > 0) structure.RemoveAt(structure.Count - 1);
+                                var last = structure.LastOrDefault();
+                                if (!(last is null)) structure.RemoveAt(structure.Count - 1);
+                                if (last?.Kind == CodeStructureKind.@class)
+                                {
+                                    foreach (var item in last.Members)
+                                    {
+                                        await source.AddDeclareGlobal(item.name, item.type, item.initial);
+                                    }
+                                }
                                 await source.AddEnd();
                                 continue;
                             }
@@ -149,18 +165,17 @@ namespace SourceCodeGeneratorAozora
                     }
                 }
 
-                var matchDec = regDec.Match(lineOrg.Trim().Replace(".to_sjis", ""));
-                matchDec = matchDec.Success ? matchDec : regDec.Match(line);
+                var matchDec = regDec.Match(line);
 
                 if (matchDec.Success)
                 {
                     var varName = matchDec.Groups[1].Value;
-                    var varValue = matchDec.Groups[2].Value;
+                    var varValue = matchDec.Groups[2].Value.Trim();
                     varValue = Regex.Replace(varValue, @"^\((.+)\)$", "$1");
                     varValue = Regex.Replace(varValue, @"""\s*\+\s*""", "");
                     var matchText = regText.Match(varValue);
                     var matchReg = regReg.Match(varValue);
-                    var matchReg2 = regReg2.Match(varValue);
+                    var matchReg2 = regReg2.Match(varValue + lineComment);
                     var matchJikauchi = regJikauchi.Match(varValue);
 
                     if (varValue == "{")
@@ -179,7 +194,19 @@ namespace SourceCodeGeneratorAozora
                         }
                         else
                         {
-                            await source.AddComment(line);
+                            var guessedType = GuessType(varValue);
+                            if (varName.StartsWith("@"))
+                            {
+                                varName = varName.Substring(1);
+                                structure.CurrentClass?.AddMember(varName, guessedType.type);
+                                await source.AddSubstitution(varName, guessedType.value);
+                                continue;
+                            }
+                            {
+                                var success = structure.LastOrDefault()?.AddMember(varName, guessedType.type) ?? false;
+                                if(success) await source.AddDeclare(varName, guessedType.value, guessedType.type);
+                                else await source.AddSubstitution(varName, guessedType.value);
+                            }
                             continue;
                         }
                     }
@@ -187,23 +214,24 @@ namespace SourceCodeGeneratorAozora
                     {
                         if (matchText.Success)
                         {
-                            await source.AddDeclareGlobalConstAuto(varName, matchText.Groups[1].Value);
+                            await source.AddDeclareGlobalAuto(varName, matchText.Groups[1].Value);
                             continue;
                         }
                         else if (matchReg2.Success)
                         {
-                            await source.AddDeclareGlobalConstAuto(varName, matchReg2.Groups[1].Value);
+                            await source.AddDeclareGlobalAuto(varName, matchReg2.Groups[1].Value);
                             continue;
                         }
                         else if (matchReg.Success)
                         {
-                            await source.AddDeclareGlobalConstAuto(varName, matchReg.Groups[1].Value);
+                            await source.AddDeclareGlobalAuto(varName, matchReg.Groups[1].Value);
                             continue;
                         }
                         else if (matchJikauchi.Success)
                         {
-                            await source.AddDeclareGlobalConstChar(varName,
+                            await source.AddDeclareGlobalChar(varName,
                                 GetStringFromByteStringNibbleReverse(matchJikauchi.Groups[1].Value)
+                                , "const "
                                 );
                             continue;
                         }
@@ -233,6 +261,18 @@ namespace SourceCodeGeneratorAozora
             return sjis.GetString(result);
         }
 
+        public static (string type, string value) GuessType(string statement)
+        {
+            Match m;
+            if (statement == "nil") return ("dynamic", "null");
+            if (statement == "[]") return ("List<string>", "new List<string>()");
+            if (statement == "{}") return ("Dictionary<string,string>", "new Dictionary<string,string>()");
+            if (statement == "true" || statement == "false") return ("bool", statement);
+            if (Regex.IsMatch(statement, @"^\d+$")) return ("int", statement);
+            if ((m = Regex.Match(statement, @"^:(\w+)$")).Success) return ("string", $"\"{m.Groups[1].Value}\"");
+            return ("dynamic", statement);
+        }
+
         public class CodeStructure : List<CodeStructureItem>
         {
             public void Add(CodeStructureKind kind, string title)
@@ -243,9 +283,24 @@ namespace SourceCodeGeneratorAozora
 
             public bool IsInDef => this.Any(a => a.Kind == CodeStructureKind.def);
             public bool IsLastHash => this.Count > 0 && this.Last().Kind == CodeStructureKind.hash;
+
+            public CodeStructureItem CurrentClass => this.LastOrDefault(a => a.Kind == CodeStructureKind.@class);
         }
 
-        public record CodeStructureItem(CodeStructureKind Kind, string Title);
+        public record CodeStructureItem(CodeStructureKind Kind, string Title)
+        {
+            public string[] Args { get; init; }
+            public List<CodeStructureItemMember> Members { get; } = new List<CodeStructureItemMember>();
+
+            public bool AddMember(string name, string type, string initial = null)
+            {
+                if (Members.Any(a => a.name == name)) return false;
+                Members.Add(new CodeStructureItemMember(name, type, initial));
+                return true;
+            }
+        };
+
+        public record CodeStructureItemMember(string name, string type, string initial);
 
         public enum CodeStructureKind
         {
@@ -275,36 +330,36 @@ namespace aozora2html
 
             public async Task AddComment(string content)
             {
-                await AddWithIndent($"//{content.Replace("//", "")}");
+                await Add($"//{content.Replace("//", "")}");
             }
 
             public async Task AddIf(string condition)
             {
-                await AddWithIndent($"if ({condition})");
-                await AddWithIndent("{");
+                await Add($"if ({condition})");
+                await Add("{");
                 CurrentLevel++;
             }
 
             public async Task AddIfElseIf(string condition)
             {
-                await AddWithIndent("}");
-                await AddWithIndent($"else if ({condition})");
-                await AddWithIndent("{");
+                await Add("}");
+                await Add($"else if ({condition})");
+                await Add("{");
             }
 
             public async Task AddIfElse()
             {
                 CurrentLevel--;
-                await AddWithIndent("}");
-                await AddWithIndent($"else");
-                await AddWithIndent("{");
+                await Add("}");
+                await Add($"else");
+                await Add("{");
                 CurrentLevel++;
             }
 
             public async Task AddClass(string className)
             {
-                await AddWithIndent($"public class {className}");
-                await AddWithIndent("{");
+                await Add($"public class {className}");
+                await Add("{");
                 CurrentLevel++;
             }
 
@@ -314,11 +369,11 @@ namespace aozora2html
                 var sb = new StringBuilder();
                 if (method.EndsWith("?")) sb.Append($"public bool {method.Replace("?", "")}(");
                 else sb.Append($"public dynamic {method}(");
-                sb.Append(string.Join(",", args.Select(a => $"dynamic {a}")));
+                sb.Append(string.Join(", ", args.Select(a => $"dynamic {a}")));
                 sb.Append(")");
-                await AddWithIndent("");
-                await AddWithIndent(sb.ToString());
-                await AddWithIndent(@"{");
+                await Add("");
+                await Add(sb.ToString());
+                await Add(@"{");
                 CurrentLevel++;
 
                 var val = new Dictionary<string, string>()
@@ -330,60 +385,75 @@ namespace aozora2html
 
             public async Task AddDeclareGlobalConstDictionaryStart(string name)
             {
-                await AddWithIndent("");
-                await AddWithIndent($"public const Dictionary<string, string>() {name} = new Dictionary<string, string>()");
-                await AddWithIndent($"{{");
+                await Add("");
+                await Add($"public const Dictionary<string, string>() {name} = new Dictionary<string, string>()");
+                await Add($"{{");
                 CurrentLevel++;
             }
 
             public async Task AddDeclareGlobalConstDictionaryEntry(string key, string value)
             {
-                await AddWithIndent($"{{\"{key}\", \"{value}\"}},");
+                await Add($"{{\"{key}\", \"{value}\"}},");
             }
 
             public async Task AddDeclareGlobalConstDictionaryEnd()
             {
                 CurrentLevel--;
-                await AddWithIndent("};");
+                await Add("};");
             }
 
 
-            public async Task AddDeclareGlobalConstChar(string name, string value)
+            public async Task AddDeclareGlobalChar(string name, string value, string option = "")
             {
-                await AddWithIndent($"public const char {name} = '{value}';");
+                await AddDeclareGlobal(name, "char", $"'{value}'", option);
             }
 
-            public async Task AddDeclareGlobalConstString(string name, string value)
+            public async Task AddDeclareGlobalString(string name, string value, string option = "")
             {
-                await AddWithIndent($"public const string {name} = \"{value}\";");
+                await AddDeclareGlobal(name, "string", $"\"{value}\"", option);
             }
 
-            public async Task AddDeclareGlobalConstRegex(string name, string value)
+            public async Task AddDeclareGlobalRegex(string name, string value, string option = "")
             {
-                await AddWithIndent($"public const Regex {name} = new Regex(@\"{value}\");");
+                await AddDeclareGlobal(name, "Regex", $"new Regex(@\"{value}\")", option);
             }
 
-            public async Task AddDeclareGlobalConstAuto(string name, string value)
+            public async Task AddDeclareGlobalAuto(string name, string value)
             {
-                if (name.StartsWith("PAT_")) await AddDeclareGlobalConstRegex(name, value);
-                else if (value.Length == 1) await AddDeclareGlobalConstChar(name, value);
-                else await AddDeclareGlobalConstString(name, value);
+                string option = Regex.IsMatch(name, "^[A-Z]") ? "const " : "";
+                if (name.StartsWith("PAT_")) await AddDeclareGlobalRegex(name, value, option);
+                else if (value.Length == 1) await AddDeclareGlobalChar(name, value, option);
+                else await AddDeclareGlobalString(name, value, option);
+            }
 
+            public async Task AddDeclareGlobal(string name, string type, string value, string option = "")
+            {
+                if (string.IsNullOrEmpty(value)) await Add($"public {option}{type} {name};");
+                else await Add($"public {option}{type} {name} = {value};");
             }
 
             public async Task AddDeclareString(string name, string value)
             {
-                await AddWithIndent($"string {name} = \"{value}\";");
+                await AddDeclare(name, $"\"{value}\"", "string");
             }
 
+            public async Task AddDeclare(string name, string value, string type)
+            {
+                await Add($"{type} {name} = {value};");
+            }
+
+            public async Task AddSubstitution(string name, string value)
+            {
+                await Add($"{name} = {value};");
+            }
 
             public async Task AddEnd()
             {
                 CurrentLevel--;
-                await AddWithIndent("}");
+                await Add("}");
             }
 
-            public async Task AddWithIndent(string text)
+            public async Task Add(string text)
             {
                 await streamWriter.WriteLineAsync($"{new string(' ', SpacePerLevel * Math.Max(CurrentLevel, 0))}{text}");
             }
