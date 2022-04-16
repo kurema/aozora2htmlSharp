ed225bfe53ded3649ea1f771f2d2b6e9c87c2461
kurema <kurema_makoto_software@yahoo.co.jp>
2022-04-16 23:40:23 +0900

    More tags

---
+++ aozora2html/aozora2html/Helpers/Tag/ReferenceMentioned.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+
+namespace Aozora.Helpers.Tag;
+
+/*
+    # 参照先用
+    #
+    # 前方参照でこいつだけは中身をチェックする
+    # 子要素を持つAozora2Html::Tag::Inlineは全てこいつのサブクラス
+*/
+public class ReferenceMentioned : Inline
+{
+    //rubocop:disable Lint/MissingSuper
+    public ReferenceMentioned(object? target)
+    {
+        if (target is null || block_element(target)) return;
+        syntax_error();
+    }
+
+    public object? target { get; }
+
+    public static bool block_element(object elt)
+    {
+        switch (elt)
+        {
+            case string text: return text.Contains("<div");
+            //kurema:ここは要修正？
+            case System.Collections.IEnumerable enumerable:
+                foreach (var item in enumerable)
+                {
+                    if (block_element(item)) return true;
+                }
+                return true;
+            default: return elt is ReferenceMentioned;
+        }
+    }
+
+    //kurema:挙動に自信がない。
+    public string target_string
+    {
+        get
+        {
+            switch (target)
+            {
+                case string text: return text;
+                case ReferenceMentioned reference: return reference.target_string;
+                case System.Collections.IEnumerable enumerable:
+                    {
+                        var sb = new StringBuilder();
+                        foreach (var item in enumerable)
+                        {
+                            switch (item)
+                            {
+                                case ReferenceMentioned reference:
+                                    sb.Append(reference.target_string);
+                                    break;
+                                default: sb.Append(item.ToString() ?? "");
+                                    break;
+                            }
+                        }
+                        return sb.ToString();
+                    }
+                default:return target?.ToString() ?? "";
+            }
+        }
+    }
+}
--- aozora2html/aozora2html/Helpers/Tag/Tag.cs
+++ aozora2html/aozora2html/Helpers/Tag/Tag.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Aozora.Helpers.Tag;
@@ -21,6 +20,46 @@ public interface ICharTypeProvider
     public CharType char_type { get; }
 }
 
+public interface IHtmlProvider
+{
+    public string to_html();
+
+}
+
+/*
+require_relative 'tag/accent'
+require_relative 'tag/gaiji'
+require_relative 'tag/embed_gaiji'
+require_relative 'tag/un_embed_gaiji'
+require_relative 'tag/editor_note'
+require_relative 'tag/oneline_indent'
+require_relative 'tag/multiline'
+require_relative 'tag/multiline_style'
+require_relative 'tag/font_size'
+require_relative 'tag/jizume'
+require_relative 'tag/keigakomi'
+require_relative 'tag/multiline_yokogumi'
+require_relative 'tag/multiline_caption'
+require_relative 'tag/multiline_midashi'
+require_relative 'tag/jisage'
+require_relative 'tag/oneline_jisage'
+require_relative 'tag/multiline_jisage'
+require_relative 'tag/oneline_chitsuki'
+require_relative 'tag/multiline_chitsuki'
+require_relative 'tag/midashi'
+require_relative 'tag/ruby'
+require_relative 'tag/kunten'
+require_relative 'tag/kaeriten'
+require_relative 'tag/okurigana'
+require_relative 'tag/inline_keigakomi'
+require_relative 'tag/inline_yokogumi'
+require_relative 'tag/inline_caption'
+require_relative 'tag/inline_font_size'
+require_relative 'tag/decorate'
+require_relative 'tag/dir'
+require_relative 'tag/img'
+ */
+
 public class Tag : ICharTypeProvider
 {
     public string inspect() { return ToString() ?? ""; }
@@ -39,11 +78,10 @@ public class Block : Tag, IClosable
 {
     public Block(Aozora2Html parser) : base()
     {
-        //block_allowed_contextを実装後次の行をコメントインしてください。
         if (!parser.block_allowed_context) syntax_error();
     }
 
-    public string close_tag() => "</div>";
+    public virtual string close_tag() => "</div>";
 }
 
 public class Indent : Block
@@ -53,7 +91,7 @@ public class Indent : Block
     }
 }
 
-public class Chitsuki : Indent
+public class Chitsuki : Indent, IHtmlProvider
 {
     public int length { get; }
 
@@ -62,14 +100,14 @@ public class Chitsuki : Indent
         this.length = length;
     }
 
-    public override string ToString()
+    public string to_html()
     {
         return $"<div class=\"chitsuki_{length}\" style=\"text-align:right; margin-right: {length}em\">";
     }
 }
 
 //濁点つきカタカナ用
-public class DakutenKatakana : Inline
+public class DakutenKatakana : Inline, IHtmlProvider
 {
     public DakutenKatakana(int num, string katakana, string gaiji_dir)
     {
@@ -87,7 +125,7 @@ public class DakutenKatakana : Inline
     //kurema:これは後で実装。
     //using StringRefinements
 
-    public override string ToString()
+    public string to_html()
     {
         //kurema:これはU+FF9Eで実現できるのでは？
         //kurema:まぁでもsjis変換で壊れるようなことはしない方が良いか。
