46d328e9b0e9df2b458cd58ba9ef8e504eaed8d8
kurema <kurema_makoto_software@yahoo.co.jp>
2022-04-30 15:25:39 +0900

    Update README.md...

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ C#で[aozora2html](https://github.com/aozorahack/aozora2html)。
 ## 手順
 1. SourceCodeGeneratorAozoraで可能な部分だけ書き換え(微妙)。
 2. 手作業でC#に書き換え。その他必要なコードを追加。
-3. デバッグ。テスト作成。←今ここ
-4. ネームスキームその他をC#風に改訂。
+3. デバッグ。テスト作成。
+4. CUI作成。EPUB作成対応。←今ここ
+5. ネームスキームその他をC#風に改訂。
 
 ## ライセンス
 MITライセンス。  
-aozora2htmlがCC0なので、要望があればCC0にすることも考えます。
--- aozora2html/aozora2htmlConsole/Program.cs
+++ aozora2html/aozora2htmlConsole/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+using System.CommandLine.Invocation;
+
+// See https://aka.ms/new-console-template for more information
+
+var rootCommand = new RootCommand("青空文庫をどうのこうのするコマンドです。")
+{
+    new Option("--gaiji-dir","setting gaiji directory"),
+};
+
+await rootCommand.InvokeAsync(args);
