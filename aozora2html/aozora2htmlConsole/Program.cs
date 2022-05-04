a0735e1d566c9a5f6ab2ca2be90bac9005fec066
kurema <kurema_makoto_software@yahoo.co.jp>
2022-05-04 20:09:06 +0900

    Update Program.cs

--- aozora2html/aozora2htmlConsole/Program.cs
+++ aozora2html/aozora2htmlConsole/Program.cs
@@ -3,21 +3,28 @@ using System.CommandLine.Invocation;
 
 // See https://aka.ms/new-console-template for more information
 
-var rootCommand = new RootCommand("Usage: aozora2html [options] <text file> [<html file>]")
+var optionGaiji = new Option<DirectoryInfo?>("--gaiji-dir", () => null, "setting gaiji directory");
+var optionCss = new Option<string[]>("--css-files", () => Array.Empty<string>(), "setting css directory") { Arity = ArgumentArity.ZeroOrMore };
+var optionJisx = new Option<bool>("--use-jisx0213", () => false, "use JIS X 0213 character") { Arity = new ArgumentArity(0, 1) };
+var optionUnicode = new Option<bool>("--use-unicode", () => false, "use Unicode character") { Arity = new ArgumentArity(0, 1) };
+var argumentIn = new Argument<string>("text file", "input file.");
+var argumentOut = new Argument<FileInfo?>("html file", () => null, "output file").LegalFilePathsOnly();
+//new Option<bool>("--error-utf8","show error messages in UTF-8, not Shift_JIS"),
+
+var rootCommand = new RootCommand()
 {
-    new Option<DirectoryInfo>("--gaiji-dir","setting gaiji directory"),
-    new Option<DirectoryInfo>("--css-files","setting css directory"),
-    new Option<bool>("--use-jisx0213","use JIS X 0213 character"),
-    new Option<bool>("--use-unicode","use Unicode character"),
-    new Option<bool>("--error-utf8","show error messages in UTF-8, not Shift_JIS"),
+    optionGaiji,
+    optionCss,
+    optionJisx,
+    optionUnicode,
+    argumentIn,
+    argumentOut,
 };
 
-rootCommand.AddArgument(new Argument<FileInfo>("text file", "input file."));
-rootCommand.AddArgument(new Argument("html file", "output file.") );
-
-rootCommand.SetHandler(() =>
+rootCommand.SetHandler((DirectoryInfo? gaijiDir, string[] cssFiles, bool useJisx0213, bool useUnicode, string textFile, FileInfo? htmlFile) =>
 {
+    Console.WriteLine($"jisx:{useJisx0213}\nunicode:{useUnicode}");
+}, optionGaiji, optionCss, optionJisx, optionUnicode, argumentIn, argumentOut);
 
-});
-
-await rootCommand.InvokeAsync(args);
+//await rootCommand.InvokeAsync(args);
+await rootCommand.InvokeAsync("test --css-files aaa --use-unicode");
