023d94cfd5491c05021149fb9cdb2d377f760f3d
kurema <kurema_makoto_software@yahoo.co.jp>
2022-05-09 15:29:50 +0900

    CRLFCheck

---
+++ tools/CRLFCheck/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+// See https://aka.ms/new-console-template for more information
+
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+using var sr = new StreamReader(args[0],Encoding.GetEncoding("Shift_JIS"));
+var content = sr.ReadToEnd();
+
+var result= new Regex(@"([^\r\n]+[^\r])(\n)|([^\r\n]+)(\r)[^\n]").Matches(content);
+foreach(Match item in result)
+{
+    Console.WriteLine(item.Groups[2].Value.Replace("\r", @"\r").Replace("\n", @"\n") + " found");
+    Console.WriteLine($"{item.Groups[1].Value} <- Here");
+}
