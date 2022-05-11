979c9c84363306bcd207802c8832d21699b4a114
kurema <kurema_makoto_software@yahoo.co.jp>
2022-05-11 21:06:23 +0900

    GaijiChuki

---
+++ tools/gaiji_chuki/GaijiChukiConvert/GaijiChukiConvert/ChuukiReader.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Text.RegularExpressions;
+
+
+namespace GaijiChukiConvert;
+
+public class ChuukiReader
+{
+    StreamReader Reader;
+
+
+    public ChuukiReader(StreamReader reader)
+    {
+        Reader = reader ?? throw new ArgumentNullException(nameof(reader));
+    }
+
+    public async void Process()
+    {
+        bool star = false;
+         while (true)
+        {
+            var line = await Reader.ReadLineAsync();
+
+            if (line == null) break;
+
+            while (line.Contains('［') && line.Contains('］'))
+            {
+                line += await Reader.ReadLineAsync();
+            }
+
+            {
+                var match = Regex.Match(line, @"^(.+)【その他】に戻る[\s　]*$");
+                if (match.Success)
+                {
+                    break;
+                }
+            }
+
+            if (line.Contains('★')) star = true;
+
+            {
+                var match = Regex.Match(line, @"^(.+)【(.+)】[\s　]*部首・読み索引に戻る[\s　]*部首・画数索引に戻る[\s　]*$");
+                if (match.Success)
+                {
+                    Console.WriteLine($"{match.Groups[1].Value} : {match.Groups[2].Value}");
+                    continue;
+                }
+            }
+        }
+    }
+}
---
+++ tools/gaiji_chuki/GaijiChukiConvert/GaijiChukiConvert/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.RegularExpressions;
+
+// See https://aka.ms/new-console-template for more information
+
+args = new[] { "gaiji_chuki.txt" };
+
+var gaiji = new GaijiChukiConvert.ChuukiReader(new StreamReader(args[0]));
+gaiji.Process();
