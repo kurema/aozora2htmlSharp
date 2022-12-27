deea48d4bda23acf01d78de312bccfa1a72744bb
kurema <kurema_makoto_software@yahoo.co.jp>
2022-12-28 03:23:12 +0900

    JstreamString

---
+++ aozora2html/TestProject/UnitTestJstreamString.cs
@@ -0,0 +1,60 @@
+﻿using Xunit;
+using Aozora;
+
+namespace TestProject;
+
+public static class UnitTestJstreamString
+{
+    [Fact]
+    public static void TestNewError()
+    {
+        string message = "";
+        try
+        {
+            //kurema:こちらでは指定しないとCRLFチェックはしません。
+            var stream = new JstreamString("aaa\nbbb\n", true);
+        }
+        catch (Aozora.Exceptions.UseCRLFException e)
+        {
+            message = e.Message;
+        }
+        //kurema:こちらでは処理を終了しないのでメッセージが変わっています。
+        Assert.Equal("改行コードを、「CR+LF」にあらためてください", message);
+    }
+
+    [Fact]
+    public static void TestReadChar()
+    {
+        var stream = new JstreamString("aあ５\r\n％\\b\r\n");
+        Assert.Equal('a', stream.ReadChar());
+        Assert.Equal('あ', stream.ReadChar());
+        Assert.Equal('５', stream.ReadChar());
+        Assert.Equal('\n', stream.ReadChar());//kurema:char?型なので\n。\r\nではない。
+        Assert.Equal('％', stream.ReadChar());
+        Assert.Equal('\\', stream.ReadChar());
+        Assert.Equal('b', stream.ReadChar());
+        Assert.Equal('\n', stream.ReadChar());
+        Assert.Null(stream.ReadChar());//kurema:endcharではない。
+        Assert.Null(stream.ReadChar());//kurema:何度もread_charするとnullが複数回出る。
+    }
+
+    [Fact]
+    public static void TestPeekChar()
+    {
+        var stream = new JstreamString("aあ５\r\n％\\b\r\n");
+        Assert.Equal('a', stream.PeekChar(0));
+        Assert.Equal('あ', stream.PeekChar(1));
+        Assert.Equal('５', stream.PeekChar(2));
+        Assert.Equal('\n', stream.PeekChar(3));
+        //改行文字以降は正しい値を保証しない
+        Assert.Equal('a', stream.ReadChar());
+        Assert.Equal('あ', stream.PeekChar(0));
+        Assert.Equal('あ', stream.ReadChar());
+        Assert.Equal('５', stream.ReadChar());
+        Assert.Equal('\n', stream.ReadChar());
+        Assert.Equal('％', stream.PeekChar(0));
+        Assert.Equal('\\', stream.PeekChar(1));
+        Assert.Equal('b', stream.PeekChar(2));
+        Assert.Equal('\n', stream.PeekChar(3));
+    }
+}
--- aozora2html/aozora2html/JstreamString.cs
+++ aozora2html/aozora2html/JstreamString.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Aozora
@@ -13,13 +15,12 @@ namespace Aozora
 
         int _line;
         int position = 0;
-        char? current_char;
+        char? current_char => text[position] == Jstream.CR ? Jstream.LF : text[position];
         string text;
 
         public JstreamString(string input, bool strictReturnCode = false)
         {
             _line = 0;
-            current_char = null;
             text = input;
             StrictReturnCode = strictReturnCode;
 
@@ -46,10 +47,10 @@ namespace Aozora
         public char? PeekChar(int pos)
         {
             if (position + pos >= text.Length) return null;
-            var peeked= text[position + pos];
+            var peeked = text[position + pos];
             if (peeked == Jstream.CR)
             {
-                if (StrictReturnCode && (position + pos + 1 >= text.Length || text[position + pos + 1] != Jstream.LF))
+                if (StrictReturnCode && !(position + pos + 1 < text.Length && text[position + pos + 1] == Jstream.LF))
                     throw new Exceptions.UseCRLFException();
                 peeked = Jstream.LF;
             }
@@ -58,27 +59,106 @@ namespace Aozora
 
         public char? ReadChar()
         {
-            throw new NotImplementedException();
             ReadAny = true;
-            if (position >= text.Length) return null;
-            var result = text[position];
+            var char1 = GetCharAt(position);
+            var char2 = GetCharAt(position + 1);
+            if (char1 is Jstream.LF)
+            {
+                _line++;
+                if (StrictReturnCode) throw new Exceptions.UseCRLFException();
+                position++;
+                return char1;
+            }
+            if(char1 is Jstream.CR)
+            {
+                _line++;
+                if (char2 is Jstream.LF)
+                {
+                    position += 2;
+                }
+                else
+                {
+                    if (StrictReturnCode) throw new Exceptions.UseCRLFException();
+                    position++;
+                }
+                return Jstream.LF;
+            }
             position++;
-            return result;
+            return char1;
         }
 
-        public string? ReadLine()
-        {
-            throw new NotImplementedException();
-        }
+        public string? ReadLine() => ReadTo(Jstream.LF);
 
         public string? ReadTo(char endchar)
         {
-            throw new NotImplementedException();
+            //kurema:ReadOnlySpanで返した方が良さそうな雰囲気あるけど、これが呼ばれる状況は少ないので気にしなくて良さそう。
+            //kurema:なおSpanが使えるのはSystem.Text.Encoding.CodePagesがSystem.Memoryを参照しているから。
+            //kurema:String.Concat()のようなものは使えない。
+            ReadAny = true;
+            var ros = text.AsSpan().Slice(position);
+            var index = ros.IndexOfAny(Jstream.CR, Jstream.LF, endchar);
+            if (index < 0)
+            {
+                position += ros.Length;
+                if (ros.Length == 0) return null;
+                else return ros.ToString();
+            }
+            var char1 = GetCharAt(position + index);
+            var char2 = GetCharAt(position + index + 1);
+            if (char1 is Jstream.CR or Jstream.LF)
+            {
+                //kurema:ros2にはCRもLF含まれない。
+                var ros2 = ros.Slice(0, index);
+                if (char1 is Jstream.LF)
+                {
+                    if (StrictReturnCode) throw new Exceptions.UseCRLFException();
+                    position += index + 1;
+                }
+                else if (char1 is Jstream.CR)
+                {
+                    if (char2 is Jstream.LF)
+                    {
+                        position += index + 2;
+                    }
+                    else
+                    {
+                        if (StrictReturnCode) throw new Exceptions.UseCRLFException();
+                        position += index + 1;
+                    }
+                }
+                _line++;
+#if NET7_0_OR_GREATER
+                return string.Concat(ros2, Jstream.CRLF);
+#else
+                //kurema:ToString()したらReadOnlySpan使う意味がないが。
+                return ros2.ToString() + Jstream.CRLF;
+#endif
+            }
+            else
+            {
+                position += index + 1;
+                return ros.Slice(0, index + 1).ToString();
+            }
         }
 
         public void RunInitialTest()
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < text.Length; i++)
+            {
+                char ch = text[i];
+                if (ch == Jstream.CR)
+                {
+                    if (i + 1 < text.Length && text[i + 1] == Jstream.LF) return;
+                    throw new Exceptions.UseCRLFException();
+                }
+                if (ch == Jstream.LF)
+                {
+                    throw new Exceptions.UseCRLFException();
+                }
+            }
+            return;
         }
+
+        private char? GetCharAt(int position) => position < text.Length ? text[position] : null;
     }
 }
